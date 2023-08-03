c3ba3e75d25b4f1661803b964b3c3159ef175799
Casper da Costa-Luis <casper.dcl@physics.org>
2023-08-03 18:43:04 +0100

    tidy code

--- quarterly-earnings-chatbot/whisper_test.py
+++ quarterly-earnings-chatbot/whisper_test.py
@@ -1,111 +1,64 @@
-import openai
 import os
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.docstore.document import Document
-from langchain.vectorstores import Weaviate
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage
-
 import tempfile
+
+import openai
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.docstore.document import Document
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema import HumanMessage
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import VectorStore, Weaviate
 
-os.environ["OPENAI_API_KEY"] = "random-string"
+# URL API endpoints obtained from the Prem App UI
+openai.api_key = os.environ["OPENAI_API_KEY"] = "random-string"
+whisper_url = "http://127.0.0.1:10111/v1"  # audio-to-text
+embeddings = OpenAIEmbeddings(openai_api_base="http://127.0.0.1:8444/v1")  # All MiniLM L6 v2
+weaviate_url = "http://127.0.0.1:8080"  # vector store
+vicuna_api = "http://127.0.0.1:8111/v1"  # LLM
 
-embeddings = OpenAIEmbeddings(openai_api_base="http://127.0.0.1:8444/v1")
-weaviate_url = "http://127.0.0.1:8080"
 
 def convert_audio_to_text(audio_file_path) -> str:
-    openai.api_base = "http://127.0.0.1:10111/v1"
-    openai.api_key = "random-string"
-
-    audio_file = open(audio_file_path, "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    return transcript.get('text')
+    openai.api_base = whisper_url
+    with open(audio_file_path, "rb") as audio_file:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        return transcript.get("text")
 
 
-def convert_text_to_embedding(text: str):
-    doc_result = embeddings.embed_documents([text])
-    embedding = doc_result[0]
-    return embedding
-
-def create_chunks(text_blob: str):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=20,
-        length_function=len
-    )
-
-    texts = text_splitter.create_documents([text_blob])
-    return texts
+def create_chunks(text: str):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20, length_function=len)
+    return text_splitter.create_documents([text])
 
 
-def add_to_vectorstore(texts):
-    embeddings = OpenAIEmbeddings(openai_api_base="http://127.0.0.1:8444/v1")
-    documents = []
-    for t in texts:
-        doc = Document(page_content=t.page_content)
-        documents.append(doc)
-
-    vectorstore = Weaviate.from_documents(
-        documents,
-        embeddings,
-        weaviate_url=weaviate_url,
-        by_text=False,
-    )
-    return vectorstore
+def add_to_vectorstore(texts) -> VectorStore:
+    documents = [Document(page_content=t.page_content) for t in texts]
+    return Weaviate.from_documents(documents, embeddings, weaviate_url=weaviate_url, by_text=False)
 
 
-def query_vectorstore(query: str, vectorstore):
-    embeddings = OpenAIEmbeddings(openai_api_base="http://127.0.0.1:8444/v1")
-    context = ""
+def query_vectorstore(query: str, vectorstore: VectorStore) -> str:
     query_vector = embeddings.embed_query(query)
     docs = vectorstore.similarity_search_by_vector(query_vector, k=1)
-    for doc in docs:
-        context += doc.page_content
-    return context
+    return ", ".join(doc.page_content for doc in docs)
 
 
-st.title('Chat with audio files')
+chat = ChatOpenAI(openai_api_base=vicuna_api, max_tokens=256)
+st.title("Chat with audio files")
 with st.sidebar:
-    upload_file = st.file_uploader("Choose a file")
+    upload_file = st.file_uploader("Choose an audio file")
     if upload_file is not None:
-        print(upload_file)
         with tempfile.NamedTemporaryFile() as tmp:
             tmp.write(upload_file.read())
-            text_blob = convert_audio_to_text(tmp.name)
+            st.write("Converting audio to text...")
+            text = convert_audio_to_text(tmp.name)
+        st.write("... done!")
+        chunks = create_chunks(text)
+        vector_db = add_to_vectorstore(chunks)
 
-            print("text blob converted!")
-            chunks = create_chunks(text_blob)
-            print("created chunks!")
-            vector_db = add_to_vectorstore(chunks)
-            print("added to vector db!")
-
-user_input = st.text_input('Enter your question here...')
+user_input = st.text_input("Enter your question here...")
 if user_input:
-
     context = query_vectorstore(user_input, vector_db)
-    print("context: \n")
-    print(context)
-    print("\n")
-
-    chat = ChatOpenAI(openai_api_base="http://127.0.0.1:8111/v1", max_tokens=256)
-
-    content = "Use the context below to answer the question.\n" \
-              f"Context: {context}. \n" \
-              f"Question: {user_input}"
-
-    messages = [
-        HumanMessage(content=content)
-    ]
-    print("generating...")
+    content = f"Use the context below to answer the Question.\n\nContext: {context}.\n\nQuestion: {user_input}"
+    messages = [HumanMessage(content=content)]
+    st.write("Generating...")
     res = chat(messages)
-    print(res)
     st.write(res.content)
-
-
-
-
-
-
-
